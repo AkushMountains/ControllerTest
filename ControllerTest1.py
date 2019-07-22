abc3e2f8ce015c67b9d6a07920f09db3929c1cd0
AkushMountains <25161277+AkushMountains@users.noreply.github.com>
2019-07-22 16:57:42 -0400

    Update ControllerTest1.py

--- ControllerTest1.py
+++ ControllerTest1.py
@@ -1,39 +1,55 @@
 import pyvisa as visa
 import time
-visa.log_to_screen() #allows user to see the backend calls being made to the pyvisa class
+
+# visa.log_to_screen() #allows user to see the backend calls being made to the pyvisa class
+#^^essentially a built in debugger tool for the pyvisa package
+
 
 visa.ResourceManager('@py') #creates t2he backend of library in case it wasn't already created
 rm = visa.ResourceManager()
 print(rm)
 device = rm.list_resources() #returns a tuple of the connected instruments
-print(device)
-#maybe add a line to verify the if ""== "main": ??
-
+print(device) #*prints the Serial port as still being active for some reason?*
 
 SI = rm.open_resource(device[1]) #opens GPIB port and assigns the instrument in address 5 to an pyvisa object
-print(len(device)) #prints the number of devices connected to the laptop
 
-print(SI) #prints the type of item this program creates
+print(SI) #prints the type of item this program creates "GPIBInstrument at GPIB::5::INSTR"
+
 
 SI.write('*IDN?')
-time.sleep(1)
+time.sleep(1) #on top of the built in delay the pyvisa object has, added a one second delay (which should be more than enough) for the system to properly recognize the command.
 name = SI.read()
-print(name)
+print(name) #prints "TDKRF, SI-300 w/SCAN,0,3.25"
 
-# SI.write('SI:MENU')
+SI.write('SI:MENU') #changes screens, executes successfully
 time.sleep(1)
 
+#
+SI.write('SI:CONFIG4:CH1') #module 4 is the x-axis as per the documentation provided to me by TDKRF
+time.sleep(1)
+SI.write('SI:CONFIG5:CH2') #module 5 is the y-axis
+time.sleep(1)
 
-SI.write('SI:CONFIG4:CH1')
+SI.write('SI:CH1') #SHOULD change screen to go to X-Y positioner. FAILS
+time.sleep(1)
+print(SI.read()) #prints "INVALID CHANNEL"
+
+SI.write('SI:Q:CH1') #SHOULD return which module channel 1 is connected to(module 4), but FAILS
+time.sleep(1)
+print(SI.read())  #prints "INVALID CHANNEL"
+
+
+
+
+#below this line are more commands SI-300 can take, nothing of concern as of right now.
+############################################################################
+
 # SI.write('MC:GO+020:CH1')
 # print(SI.query('MC:PP:CH1'))
 # SI.write('SI:CONFIG5:CH2')
 # SI.write('SI:MODULE')
 # SI.query('SI:Q:CH1')
 # SI.write('SI:CH1')
-
-#below this line is all the commands used to make the instrument do things, will cause error if python cannot communicate with the device
-############################################################################
 
 # SI.write('*IDN?')
 
@@ -64,7 +80,6 @@ SI.write('SI:CONFIG4:CH1')
 
 #SET POSITIONS
 # SI.write('MC:CP+100.0:CH1')  #CHANNEL 1 IS THE X AXIS
-# SI.write('MC:RXA555:CH1') #SETS X AXIS RATIO TO +5.55
 # SI.write('SI:CH1') #SWITCHES BETWEEN THE DIFFERENT MODULES IN THE CHANNEL SPECIFIED
 
 #SI.query('*RST') #RESETS THE ENTIRE SYSTEM INTERFACE CONTROLLER
